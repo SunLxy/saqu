eed607df383cc81b3edcd155d04bafe4f73ab09a
SunLxy <1011771396@qq.com>
2023-04-07 18:14:22 +0800

    fix:优化loader默认配置

--- core/src/rspack.config/config/modules.ts
+++ core/src/rspack.config/config/modules.ts
@@ -1,7 +1,10 @@
 /**
  * rspack modules 配置
  */
-import { RspackOptions } from '@rspack/core';
+import { RspackOptions, RuleSetRules } from '@rspack/core';
+interface FefaultRulesProps {
+  imageInlineSizeLimit: number;
+}
 
 export const getRspackModolesConfig = (
   env: 'development' | 'production',
@@ -12,52 +15,54 @@ export const getRspackModolesConfig = (
   const newModules = { ...module };
   return {
     ...newModules,
-    rules: [
-      {
-        test: /\.js$/,
-        type: 'jsx',
-      },
-      {
-        test: /\.ts$/,
-        type: 'tsx',
-      },
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        parser: {
-          dataUrlCondition: {
-            // Modules less than or equal to 4kb and ending in `.png` will be Base64 encoded
-            maxSize: imageInlineSizeLimit,
-          },
-        },
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: require.resolve('@svgr/webpack'),
-            options: {
-              prettier: false,
-              svgo: false,
-              svgoConfig: {
-                plugins: [{ removeViewBox: false }],
-              },
-              titleProp: true,
-              ref: true,
-            },
-          },
-          {
-            loader: require.resolve('file-loader'),
-            options: {
-              name: 'static/media/[name].[hash].[ext]',
-            },
-          },
-        ],
-        issuer: {
-          and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-        },
-      },
-      ...(newModules?.rules || []),
-    ],
+    rules: [...defaultModolesRules({ imageInlineSizeLimit }), ...(newModules?.rules || [])],
   };
 };
+
+/**
+ * 默认 loader 配置
+ */
+export const defaultModolesRules = (props: FefaultRulesProps): RuleSetRules => [
+  {
+    test: /\.js$/,
+    type: 'jsx',
+  },
+  {
+    test: /\.ts$/,
+    type: 'tsx',
+  },
+  {
+    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+    parser: {
+      dataUrlCondition: {
+        // Modules less than or equal to 4kb and ending in `.png` will be Base64 encoded
+        maxSize: props.imageInlineSizeLimit,
+      },
+    },
+    type: 'asset/resource',
+  },
+  {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: require.resolve('@svgr/webpack'),
+        options: {
+          prettier: false,
+          svgo: false,
+          svgoConfig: {
+            plugins: [{ removeViewBox: false }],
+          },
+          titleProp: true,
+          ref: true,
+        },
+      },
+      {
+        loader: require.resolve('file-loader'),
+        options: {
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      },
+    ],
+    issuer: { and: [/\.(ts|tsx|js|jsx|md|mdx)$/] },
+  },
+];
