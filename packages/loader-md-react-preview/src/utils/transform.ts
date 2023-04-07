ddb1777f9a113d790aa928eacc06400c474ccfa5
SunLxy <1011771396@qq.com>
2023-04-07 08:53:33 +0800

    fix(md):修复plugin使用

--- packages/loader-md-react-preview/src/utils/transform.ts
+++ packages/loader-md-react-preview/src/utils/transform.ts
@@ -1,20 +1,12 @@
 import {
   transformSync,
-  parseSync,
   Options as SWCoptions,
-  printSync,
   VariableDeclaration,
   VariableDeclarator,
   ClassDeclaration,
   Program,
 } from '@swc/core';
 
-/**
- * 临时解决办法
- *
- * 添加 plugin 插件报错，没找到解决办法
- *
- * **/
 const replaceStr = (span: VariableDeclarator['span'], expression: VariableDeclarator['init']): VariableDeclaration => ({
   type: 'VariableDeclaration',
   kind: 'const',
@@ -52,7 +44,7 @@ export const getTransformValue = (str: string, filename: string) => {
   const options: SWCoptions = {
     filename,
     jsc: {
-      target: 'es5', // 输出js的规范
+      target: 'esnext', // 输出js的规范
       parser: {
         // 除了 ecmascript，还支持 typescript
         syntax: 'typescript',
@@ -65,18 +57,20 @@ export const getTransformValue = (str: string, filename: string) => {
     },
     module: { type: 'commonjs' },
     minify: true,
+    plugin: (m) => {
+      const { body, ...rest } = m;
+      const newBody = body.map((item) => {
+        if (item.type === 'ExportDefaultDeclaration' && item.decl) {
+          return replaceClassStr(item.decl as unknown as ClassDeclaration);
+        } else if (item.type === 'ExportDefaultExpression') {
+          const { span, expression } = item;
+          return replaceStr(span, expression);
+        }
+        return item;
+      });
+      return { ...rest, body: newBody } as Program;
+    },
   };
-  const ast = parseSync(str, options.jsc.parser);
-  const newBody = ast.body.map((item) => {
-    if (item.type === 'ExportDefaultDeclaration' && item.decl) {
-      return replaceClassStr(item.decl as unknown as ClassDeclaration);
-    } else if (item.type === 'ExportDefaultExpression') {
-      const { span, expression } = item;
-      return replaceStr(span, expression);
-    }
-    return item;
-  });
-  const newCode = printSync({ ...ast, body: newBody } as Program);
-  const res = transformSync(newCode.code, options);
+  const res = transformSync(str, options);
   return `${res.code}\nreturn BaseCode_Export__default__value;\n`;
 };
