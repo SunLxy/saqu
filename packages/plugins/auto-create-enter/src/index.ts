008823c7639bac2a8ed8c4e755677b5262c739f5
SunLxy <1011771396@qq.com>
2023-07-16 08:13:48 +0800

    feat:添加生成入口文件时，加载全局样式

--- packages/plugins/auto-create-enter/src/index.ts
+++ packages/plugins/auto-create-enter/src/index.ts
@@ -2,7 +2,7 @@ import { Compiler } from '@rspack/core';
 import FS from 'fs-extra';
 import path from 'path';
 import { getMainCode, RouteType } from './utils';
-
+import chokidar from 'chokidar';
 /**
  * 自动生成入口文件
  */
@@ -11,6 +11,12 @@ class AutoCreateEnter {
   routeType: RouteType = 'Hash';
   /**是否把 path==="/" 当成根路径 */
   rootRoutes?: boolean | string;
+  /**主体内容*/
+  mainContent?: string;
+  /**全局样式文件地址*/
+  globalStylePath: string = path.join(process.cwd(), 'src', 'global.css');
+  /**入口文件地址*/
+  mainFilePath: string = path.join(process.cwd(), 'src', '.cache', 'main.jsx');
 
   constructor(props?: { routeType?: RouteType; rootRoutes?: boolean | string }) {
     if (props && props.routeType) {
@@ -20,22 +26,43 @@ class AutoCreateEnter {
       this.rootRoutes = props.rootRoutes;
     }
   }
+  /**监听文件*/
+  watch = () => {
+    const watch = chokidar.watch(this.globalStylePath);
+    watch.on('add', this._create);
+    watch.on('unlink', this._create);
+  };
 
-  _create() {
-    /**获取配置代码*/
-    const code = getMainCode(this.routeType, this.rootRoutes);
+  _checkStyle = () => {
+    if (FS.existsSync(this.globalStylePath)) {
+      return `import "@/global.css";\n`;
+    }
+    return '';
+  };
+
+  _create = () => {
+    if (!this.mainContent) {
+      /**获取配置代码*/
+      this.mainContent = getMainCode(this.routeType, this.rootRoutes);
+    }
+    let content = this.mainContent;
+    const cssImport = this._checkStyle();
+    if (cssImport) {
+      content = cssImport + content;
+    }
     /**获取写入文件路径*/
-    const writeFilePath = path.join(process.cwd(), 'src', '.cache', 'main.jsx');
-    /**初始化文件*/
-    FS.ensureFileSync(writeFilePath);
+    if (!FS.existsSync(this.mainFilePath)) {
+      /**初始化文件*/
+      FS.ensureFileSync(this.mainFilePath);
+    }
     /**写入内容*/
-    FS.writeFileSync(writeFilePath, code, { flag: 'w+', encoding: 'utf-8' });
-  }
+    FS.writeFileSync(this.mainFilePath, content, { flag: 'w+', encoding: 'utf-8' });
+  };
 
   apply(compiler: Compiler) {
     /**在开始编译之前执行，只执行一次*/
     compiler.hooks.afterPlugins.tap('AutoCreateEnter', () => {
-      this._create();
+      this.watch();
     });
   }
 }
