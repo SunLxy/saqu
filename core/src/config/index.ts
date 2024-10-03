37d0a6b80470c87f4a1d8c2e68e5769ce54e91e4
SunLxy <1011771396@qq.com>
2024-10-03 18:02:16 +0800

    feat:升级加载配置包

--- core/src/config/index.ts
+++ core/src/config/index.ts
@@ -1,19 +1,49 @@
-import load from 'carefree-proload';
-import defaultConfig from './config';
 import { SAquConfig, SAquArgvOptions } from './../interface';
+import jiti from 'jiti';
+import path from 'path';
+import FS from 'fs-extra';
+
+const ext = ['mjs', 'ts', 'js', 'cjs', 'mts', 'cts'];
+const fileNames = ['.saqurc', '.saqu', '.saqurc.config', '.saqu.config'];
+
+const CONFIG_FILES = fileNames.reduce((pre, next) => {
+  const list = ext.map((it) => `${next}.${it}`);
+  return list;
+}, []);
+
+const resolveConfigPath = (root: string = process.cwd()) => {
+  for (const file of CONFIG_FILES) {
+    const configFile = path.join(root, file);
+    if (FS.existsSync(configFile)) {
+      return configFile;
+    }
+  }
+  return null;
+};
 
 export const getLoadConfig = async (
   env: 'development' | 'production' | 'preview',
   argvOptions: SAquArgvOptions,
 ): Promise<{ loadConfig: SAquConfig; filePath: string }> => {
-  load.use(defaultConfig);
-  const config = await load('.saqu', { mustExist: false });
-  let newLoadConfig = (config || {}).raw || {};
-  if (typeof newLoadConfig === 'function') {
-    newLoadConfig = newLoadConfig(env, argvOptions);
+  let loadConfig = {};
+  const filePath = resolveConfigPath();
+  if (filePath) {
+    try {
+      const load = jiti(__filename, {
+        fsCache: false,
+        moduleCache: false,
+        interopDefault: true,
+      });
+      loadConfig = load(filePath);
+      if (typeof loadConfig === 'function') {
+        loadConfig = loadConfig(env, argvOptions);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
   return {
-    loadConfig: newLoadConfig,
-    filePath: (config || {}).filePath,
+    loadConfig,
+    filePath,
   };
 };
